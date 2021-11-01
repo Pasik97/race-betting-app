fd862828709186502e881cbd94587568a6b5e441
Pasik97 <k.pasik97@gmail.com>
2021-11-01 23:00:10 +0100

    LoaderWithOverlay tests

---
+++ src/components/LoaderWithOverlay/tests/LoaderWithOverlay.spec.tsx
@@ -0,0 +1,16 @@
+import {create } from "react-test-renderer";
+import LoaderWithOverlay from "../LoaderWithOverlay";
+
+describe('<LoaderWithOverlay />', () => {
+   it('should render divs if loading prop is set to true', () => {
+      const component = create(<LoaderWithOverlay loading />);
+
+      expect(component.toJSON()).toMatchSnapshot();
+   });
+
+   it('should null if loading prop is set to false', () => {
+      const component = create(<LoaderWithOverlay loading={false} />);
+
+      expect(component.toJSON()).toBeNull();
+   });
+});
